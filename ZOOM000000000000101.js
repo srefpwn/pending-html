6005ef03e6fb0a6af0e6595fdeab0096cf5ea6b2
srefpwn <ricsi@luxradio.hu>
2026-08-21 13:57:48 +0200

    Create ZOOM000000000000101.js

---
+++ ZOOM000000000000101.js
@@ -0,0 +1,10 @@
+{
+    "id": "ZOOM000000000000101",
+    "type": "CN1",
+    "year": 2006,
+    "source": "ZOOM000000000000101.html",
+    "title": "(\r\nSEA5E00D10600065301KBHT00\r\n)",
+    "name": "Kerékérzékelő cseréje",
+    "wiring_diagram": false,
+    "html": "<div class=\"manual-content-iframe\" style=\"width:100%;margin:0;padding:0;overflow:hidden;\"><iframe id=\"honda-iframe\" src=\"/manual/html/ZOOM000000000000101-content.html\" style=\"display:block;width:100%;height:calc(100vh - 100px);border:0;margin:0;padding:0;background:#cccccc;overflow:auto;\" frameborder=\"0\" scrolling=\"yes\" loading=\"eager\"></iframe></div>"
+}
