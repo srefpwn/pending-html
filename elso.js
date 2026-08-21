19e8a8e4edb115ad47570d5ca4c2c1d4e084bbeb
srefpwn <ricsi@luxradio.hu>
2026-08-21 13:41:41 +0200

    Create elso.js

---
+++ elso.js
@@ -0,0 +1,13 @@
+with(document) {
+write("<v:group id=\"group1\"  name=\"PrtGrpId\" class=\"drag\" style=\"position:relative; width:950px; height:463px;\" coordsize=\"950,463\">");
+write("<v:rect id=\"rect1\" name=\"PrtRectId\"class=\"drag\"  style=\"width:950px;height:463px;\">");
+write("<img id=\"img1\" name=\"PrtImgId\" src=\"../img/SEA5E00D10600065301KBHD01.PNG\" border=\"1\" class=\"drag\" style=\"position:absolute;left:0px;top:0px;width:950px;height:463px;border-color:#ffffff\">");
+write("<p id=\"p1_1\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:636px;top:425px;font-weight:normal;font-size:8.68pt;\">");
+write("<nobr><b>KERÉKÉRZÉKELŐ<br></b></nobr>");
+write("</p>");
+write("<p id=\"p1_2\" name=\"PrtPId\" class=\"drag\" style=\"position:absolute;left:660px;top:275px;font-weight:normal;font-size:8.68pt;\">");
+write("<nobr><b>6 mm CSAVAR<br>9,8 N·m (1,0 kgf·m)<br></b></nobr>");
+write("</p>");
+write("</v:rect>");
+write("</v:group>");
+}
