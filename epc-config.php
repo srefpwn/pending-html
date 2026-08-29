d779df1b1b76539d78b66d51e630245c38c9b68c
srefpwn <ricsi@luxradio.hu>
2026-08-29 23:35:12 +0200

    Create epc-config.php

---
+++ epc-config.php
@@ -0,0 +1,52 @@
+<?php
+
+
+
+$configs = [
+
+    [
+        "brand" => "honda",
+        
+    	"model" => "accord",
+        
+        "series" => "series_7",
+        
+        "model_code" => "cn1",
+
+        'name' => 'Accord 2006-08 - CN1 (2.2) - Sport',
+        
+        'preview' => '/images/types/honda/accord/series_7/cn1.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/cn1-lista.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/cn1-epc.json',
+
+        'image_dir' => '/images/epc/honda/accord/series_7/cn1/',
+
+        'extension' => 'gif',
+
+    ],
+    [
+        "brand" => "honda",
+        
+    	"model" => "accord",
+        
+        "series" => "series_7",
+        
+        "model_code" => "clr",
+
+        'name' => 'Accord 2006-08 - CL7-R (2.0) - Euro-R',
+
+        'preview' => '/images/types/honda/accord/series_7/clr.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/clr-lista.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/clr-epc.json',
+
+        'image_dir' => '/images/epc/honda/accord/series_7/clr/',
+
+        'extension' => 'png',
+
+    ]
+
+];
