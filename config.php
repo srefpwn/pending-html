1b6a7969ec30fd3f6c68259146cbbbd967939534
srefpwn <ricsi@luxradio.hu>
2026-09-12 18:05:28 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,138 @@
+<?php
+
+
+
+$configs = [
+
+    [
+        "brand" => "honda",
+        
+    	"model" => "accord",
+        
+        "series" => "series_7",
+        
+        "model_code" => "cn1",
+        
+        "body_code" => "sedan",
+        
+        "trim_code" => "sport",
+
+        'name' => 'Accord 2006-08 - CN1 (2.2) - Sport',
+        
+        'preview' => '/images/types/honda/accord/series_7/sedan/cn1-sport.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/cn1-sport/list.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/cn1-sport/epc.json',
+
+        'image_dir' => '/images/epc/honda/accord/series_7/sedan/cn1-sport/',
+
+        'extension' => 'gif',
+
+    ],
+    [
+        "brand" => "honda",
+        
+    	"model" => "accord",
+        
+        "series" => "series_7",
+        
+        "model_code" => "cl9",
+        
+        "body_code" => "sedan",
+        
+        "trim_code" => "executive-e",
+
+        'name' => 'Accord 2006-08 - CL9 (2.4) - Executive-E',
+
+        'preview' => '/images/types/honda/accord/series_7/sedan/cl9-executive.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/cl9-executive-e/list.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/cl9-executive-e/epc.json',
+
+        'image_dir' => '/images/epc/honda/accord/series_7/sedan/cl9-executive-e/',
+
+        'extension' => 'gif',
+
+    ],
+    [
+        "brand" => "honda",
+        
+    	"model" => "accord",
+        
+        "series" => "series_7",
+        
+        "model_code" => "clr",
+        
+        "body_code" => "sedan",
+        
+        "trim_code" => "smart",
+
+        'name' => 'Accord 2006-08 - CL7-R (2.0) - Euro-R',
+
+        'preview' => '/images/types/honda/accord/series_7/sedan/clr.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/clr-smart/list.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/accord/series_7/sedan/clr-smart/epc.json',
+
+        'image_dir' => '/images/epc/honda/accord/series_7/sedan/clr-smart/',
+
+        'extension' => 'png',
+
+    ],
+    [
+        "brand" => "honda",
+        
+    	"model" => "crv",
+        
+        "series" => "series_3",
+        
+        "model_code" => "re5",
+        
+        "body_code" => "suv",
+        
+        "trim_code" => "executive",
+
+        'name' => 'CR-V 2007-12 - RE5 (2.0) - Executive',
+
+        'preview' => '/images/types/honda/crv/series_3/re5-executive.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/crv/series_3/re5-executive/list.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/crv/series_3/re5-executive/epc.json',
+
+        'image_dir' => '/images/epc/honda/crv/series_3/re5-executive/',
+
+        'extension' => 'gif',
+
+    ],
+    [
+        "brand" => "honda",
+        
+    	"model" => "crv",
+        
+        "series" => "series_4",
+        
+        "model_code" => "re5",
+        
+        "body_code" => "suv",
+        
+        "trim_code" => "executive",
+
+        'name' => 'CR-V 2013-17 - RE5 (2.0) - Executive',
+
+        'preview' => '/images/types/honda/crv/series_4/re5-executive.jpg',
+
+        'lista_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/crv/series_4/re5-executive/list.json',
+
+        'epc_json' => $_SERVER['DOCUMENT_ROOT'] . '/data/epc/honda/crv/series_4/re5-executive/epc.json',
+
+        'image_dir' => '/images/epc/honda/crv/series_4/re5-executive/',
+
+        'extension' => 'png',
+
+    ]
+
+];
